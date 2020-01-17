1bbc7b6d4577b3b5681e56526dd8bcfab5573f3e
joseph704 <freelove54@naver.com>
2020-01-17 15:17:40 +0900

    Add DetailViewcontorller which shows Selected Indexpath.Row of SecondViewcontroller

--- boostCourseProjectC/DetailViewController.swift
+++ boostCourseProjectC/DetailViewController.swift
@@ -9,22 +9,47 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    var textToSetCityName:String?
+    var textToSetTemperature:Double?
+    var textToSetRainFallProbability:Int?
+    var textToSetState:Int?
+    
+    @IBOutlet weak var weatherImageView:UIImageView!
+    @IBOutlet weak var state:UILabel!
+    @IBOutlet weak var temperature:UILabel!
+    @IBOutlet weak var rainFallProbability:UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        switch textToSetState {
+        case 10:
+            weatherImageView.image=UIImage(named: "sunny")
+            state.text="맑음"
+        case 11:
+            weatherImageView.image=UIImage(named: "cloudy")
+            state.text="구름"
+        case 12:
+            weatherImageView.image=UIImage(named: "rainy")
+            state.text="비"
+        case 13:
+            weatherImageView.image=UIImage(named: "snowy")
+            state.text="눈"
+        default:
+            debugPrint("존재하지 않는 state")
+        }
+        
+        let fahrenheitDegree =  textToSetTemperature! * 1.8 + 32
+        let output = floor(fahrenheitDegree * 100) / 100
+        
+        temperature.text = "섭씨 \(textToSetTemperature!)도 / 화씨 \(output)도"
+        rainFallProbability.text = "강수확률: \(textToSetRainFallProbability!)%"
+        
+        navigationItem.title=textToSetCityName
+        
+        
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
